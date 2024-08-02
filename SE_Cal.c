4761c935a1b78cc2d916a5f33e7643d794cc5b7f
rtzeropti <84755410+rtzeropti@users.noreply.github.com>
2024-08-02 14:12:11 +0800

    Update SE_Cal.c

--- SE_Cal.c
+++ SE_Cal.c
@@ -3,6 +3,8 @@
 static unsigned char inner_array[3];
 static unsigned char outer_array[3];
 
-static void check_array_valid(unsigned char *array);
-static void check_array_valid(unsigned char *array)
-{}
+static void check_array_valid(struct obj_2D3D_t *a);
+static void check_array_valid(struct obj_2D3D_t *a)
+{
+    
+}
