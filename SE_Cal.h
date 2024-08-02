558d1e77ba243448065382a48b83e55834214454
rtzeropti <84755410+rtzeropti@users.noreply.github.com>
2024-08-02 12:18:23 +0800

    Update SE_Cal.h

--- SE_Cal.h
+++ SE_Cal.h
@@ -1,4 +1,23 @@
 #ifndef SE_CAL_H
 #define SE_CAL_H
 
+type enum
+{
+    CIRCLE      = 0x01, //Varry from 0 to 1
+    TRIANGLE    = 0x03,
+    SQUARE      = 0x04,
+    Error       = 0x05
+}obj_2D_type;
+
+type enum
+{
+    CC          = 0x02,
+    CT          = 0x04,
+    CS          = 0x05,
+    TT          = 0x06,
+    TS          = 0x07,
+    SS          = 0x08,
+    ER          = 0x09
+}obj_3D_type;
+
 #endif
