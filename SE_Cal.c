b23d926b9f33b3c080d469a32ec5788f1c9d2f56
rtzeropti <84755410+rtzeropti@users.noreply.github.com>
2024-08-02 11:48:10 +0800

    Update SE_Cal.c

--- SE_Cal.c
+++ SE_Cal.c
@@ -1,1 +1,8 @@
 #include "SE_Cal.h"
+
+static unsigned char inner_array[3];
+static unsigned char outer_array[3];
+
+static void check_array_valid(unsigned char *array);
+static void check_array_valid(unsigned char *array)
+{}
